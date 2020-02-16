db17734f7c4f5150458e6f85a91b8a4a9bc8326e
nickmcummins <nickmcummins@gmail.com>
2020-02-15 18:14:38 -0800

    Remove unnecessary static clauses

--- src/main/java/com/nickmcummins/webscraping/org/eclipsecolorthemes/ThemeConverter.java
+++ src/main/java/com/nickmcummins/webscraping/org/eclipsecolorthemes/ThemeConverter.java
@@ -8,47 +8,43 @@ import java.util.*;
 import static java.util.Map.entry;
 
 public class ThemeConverter {
-    private static final Map<String, List<String>> ECLIPSE_TO_IDEA_OPTIONS;
-    static {
-        ECLIPSE_TO_IDEA_OPTIONS = Map.of(
-                "background", List.of("CONSOLE_BACKGROUND_KEY", "GUTTER_BACKGROUND", "RIGHT_MARGIN_COLOR", "TEARLINE_COLOR"),
-                "selectionForeground", List.of("SELECTION_FOREGROUND"), "selectionBackground", List.of("SELECTION_BACKGROUND"),
-                "currentLine", List.of("CARET_ROW_COLOR"),
-                "lineNumber", List.of("CARET_COLOR", "LINE_NUMBERS_COLOR"));
-    }
-    private static final Map<String, String> ECLIPSE_TO_IDEA_ATTRIBUTES;
-    static {
-        ECLIPSE_TO_IDEA_ATTRIBUTES = Map.ofEntries(
-                entry("occurrenceIndication", "IDENTIFIER_UNDER_CARET_ATTRIBUTES"),
-                entry("writeOccurrenceIndication", "WRITE_IDENTIFIER_UNDER_CARET_ATTRIBUTES"),
-                entry("singleLineComment", "DEFAULT_LINE_COMMENT"),
-                entry("multiLineComment", "DEFAULT_BLOCK_COMMENT"),
-                entry("commentTaskTag", "TODO_DEFAULT_ATTRIBUTES"),
-                entry("javadoc", "DEFAULT_DOC_COMMENT"),
-                entry("javadocLink", "DEFAULT_DOC_COMMENT_TAG"),
-                entry("javadocTag", "DEFAULT_DOC_MARKUP"),
-                entry("javadocKeyword", "DEFAULT_DOC_COMMENT_TAG"),
-                entry("class", "DEFAULT_CLASS_NAME"),
-                entry("interface", "DEFAULT_INTERFACE_NAME"),
-                entry("method", "DEFAULT_FUNCTION_CALL"),
-                entry("methodDeclaration", "DEFAULT_FUNCTION_DECLARATION"),
-                entry("bracket", "DEFAULT_BRACKETS"),
-                entry("number", "DEFAULT_NUMBER"),
-                entry("string", "DEFAULT_VALID_STRING_ESCAPE"),
-                entry("operator", "DEFAULT_DOT"),
-                entry("keyword", "DEFAULT_KEYWORD"),
-                entry("annotation", "DEFAULT_METADATA"),
-                entry("staticMethod", "DEFAULT_STATIC_METHOD"),
-                entry("localVariableDeclaration", "DEFAULT_LOCAL_VARIABLE"),
-                entry("field", "DEFAULT_INSTANCE_FIELD"),
-                entry("staticFinalField", "STATIC_FINAL_FIELD_ATTRIBUTES"),
-                entry("enum", "ENUM_NAME_ATTRIBUTES"),
-                entry("inheritedMethod", "INHERITED_METHOD_ATTRIBUTES"),
-                entry("abstractMethod", "ABSTRACT_METHOD_ATTRIBUTES"),
-                entry("typeParameter", "TYPE_PARAMETER_NAME_ATTRIBUTES"),
-                entry("constant", "DEFAULT_CONSTANT")
-        );
-    }
+    private static final Map<String, List<String>> ECLIPSE_TO_IDEA_OPTIONS = Map.of(
+            "background", List.of("CONSOLE_BACKGROUND_KEY", "GUTTER_BACKGROUND", "RIGHT_MARGIN_COLOR", "TEARLINE_COLOR"),
+            "selectionForeground", List.of("SELECTION_FOREGROUND"),
+            "selectionBackground", List.of("SELECTION_BACKGROUND"),
+            "currentLine", List.of("CARET_ROW_COLOR"),
+            "lineNumber", List.of("CARET_COLOR", "LINE_NUMBERS_COLOR")
+    );
+    private static final Map<String, String> ECLIPSE_TO_IDEA_ATTRIBUTES = Map.ofEntries(
+            entry("occurrenceIndication", "IDENTIFIER_UNDER_CARET_ATTRIBUTES"),
+            entry("writeOccurrenceIndication", "WRITE_IDENTIFIER_UNDER_CARET_ATTRIBUTES"),
+            entry("singleLineComment", "DEFAULT_LINE_COMMENT"),
+            entry("multiLineComment", "DEFAULT_BLOCK_COMMENT"),
+            entry("commentTaskTag", "TODO_DEFAULT_ATTRIBUTES"),
+            entry("javadoc", "DEFAULT_DOC_COMMENT"),
+            entry("javadocLink", "DEFAULT_DOC_COMMENT_TAG"),
+            entry("javadocTag", "DEFAULT_DOC_MARKUP"),
+            entry("javadocKeyword", "DEFAULT_DOC_COMMENT_TAG"),
+            entry("class", "DEFAULT_CLASS_NAME"),
+            entry("interface", "DEFAULT_INTERFACE_NAME"),
+            entry("method", "DEFAULT_FUNCTION_CALL"),
+            entry("methodDeclaration", "DEFAULT_FUNCTION_DECLARATION"),
+            entry("bracket", "DEFAULT_BRACKETS"),
+            entry("number", "DEFAULT_NUMBER"),
+            entry("string", "DEFAULT_VALID_STRING_ESCAPE"),
+            entry("operator", "DEFAULT_DOT"),
+            entry("keyword", "DEFAULT_KEYWORD"),
+            entry("annotation", "DEFAULT_METADATA"),
+            entry("staticMethod", "DEFAULT_STATIC_METHOD"),
+            entry("localVariableDeclaration", "DEFAULT_LOCAL_VARIABLE"),
+            entry("field", "DEFAULT_INSTANCE_FIELD"),
+            entry("staticFinalField", "STATIC_FINAL_FIELD_ATTRIBUTES"),
+            entry("enum", "ENUM_NAME_ATTRIBUTES"),
+            entry("inheritedMethod", "INHERITED_METHOD_ATTRIBUTES"),
+            entry("abstractMethod", "ABSTRACT_METHOD_ATTRIBUTES"),
+            entry("typeParameter", "TYPE_PARAMETER_NAME_ATTRIBUTES"),
+            entry("constant", "DEFAULT_CONSTANT")
+    );
 
     public static String formatHexValue(String hexColor) {
         String formattedHex = hexColor.replaceFirst("#", "").toLowerCase();
