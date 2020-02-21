f5492afcb2d7cf77a9b2cd5a300fd9db12838086
nickmcummins <nickmcummins@gmail.com>
2020-02-20 17:01:43 -0800

    Add EclipseColorThemeDownloader class stub

---
+++ src/main/java/com/nickmcummins/webscraping/org/eclipsecolorthemes/EclipseColorThemeDownloader.java
@@ -0,0 +1,7 @@
+package com.nickmcummins.webscraping.org.eclipsecolorthemes;
+
+public class EclipseColorThemeDownloader {
+    private static final String START_PAGE = "https://web.archive.org/web/20190728065507/http://www.eclipsecolorthemes.org/?list=all&q=";
+
+
+}
