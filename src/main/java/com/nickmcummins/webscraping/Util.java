c5c4a0e0ae341d06bc248a1b7fa0f0a02d97b0e5
nickmcummins <nickmcummins@gmail.com>
2020-04-29 20:44:15 -0700

    handle RST_STREAM exceptions with retries

--- src/main/java/com/nickmcummins/webscraping/Util.java
+++ src/main/java/com/nickmcummins/webscraping/Util.java
@@ -12,14 +12,18 @@ import java.net.http.HttpResponse;
 import static java.lang.Thread.sleep;
 
 public class Util {
-    public static String get(String url) throws IOException, InterruptedException, CannotDownloadException {
+    private static final int MAX_RETRIES = 5;
+
+    public static String get(String url) throws InterruptedException, CannotDownloadException {
         HttpClient requests = HttpClient.newBuilder().build();
         HttpResponse<String> response = null;
         boolean success = false;
+        int tries = 0;
         do {
             if (response != null && response.statusCode() == 302)
                 url = response.headers().allValues("location").get(0);
             try {
+                tries += 1;
                 response = requests.send(HttpRequest.newBuilder(URI.create(url)).build(), HttpResponse.BodyHandlers.ofString());
                 if (response.statusCode() == 200)
                     success = true;
@@ -27,11 +31,15 @@ public class Util {
                     System.out.println(String.format("Received 404 attempting to download %s; skipping", url));
                     throw new CannotDownloadException();
                 }
-            } catch (ConnectException ce) {
-                System.out.println(String.format("Connection exception when attempting to download %s. Sleeping 10 seconds.", url));
-                sleep(10000);
+            } catch (IOException ce) {
+                if (ce instanceof ConnectException || ce.getMessage().equals("Received RST_STREAM: Internal error")) {
+                    System.out.println(String.format("Connection exception when attempting to download %s. Sleeping 10 seconds.", url));
+                    sleep(10000);
+                }
             }
-        } while ((response != null && response.statusCode() == 302) || !success);
+        } while ((response != null && response.statusCode() == 302) || (!success && tries < MAX_RETRIES));
+        if (response == null)
+            throw new CannotDownloadException();
 
         return response.body();
     }
