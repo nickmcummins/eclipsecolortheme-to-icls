fd78f80b1805c7f05d850b0814fccc5870a904ac
nickmcummins <nickmcummins@gmail.com>
2020-05-27 17:32:15 -0700

    randomized wait for HTTP requests. also set user-agent randomly

--- src/main/java/com/nickmcummins/webscraping/http/HttpUtil.java
+++ src/main/java/com/nickmcummins/webscraping/http/HttpUtil.java
@@ -20,17 +20,21 @@ public class HttpUtil {
     private static final List<LocalDateTime> CONNECTION_REFUSED_TIMESTAMPS = new ArrayList<>();
     private static final Duration CONNECT_REFUSED_EXPIRATION = Duration.ofMinutes(10);
     private static final int SECONDS_IN_MS = 1000;
+    private static final List<String> USER_AGENTS = List.of(
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:76.0) Gecko/20100101 Firefox/76.0",
+            "Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko"
+    );
 
     public static String get(String url) throws InterruptedException, CannotDownloadException {
         HttpClient requests = HttpClient.newBuilder().build();
         HttpResponse<String> response = null;
         boolean success = false;
         int tries = 0;
-
         do {
             removeExpiredConnectRefusedTimestamps();
             if (!CONNECTION_REFUSED_TIMESTAMPS.isEmpty()) {
-                int seconds = 60 + 30 * (CONNECTION_REFUSED_TIMESTAMPS.size()-1);
+                int seconds = (int)(Math.random() * 10) * (CONNECTION_REFUSED_TIMESTAMPS.size()-1);
                 print("\tSleeping %d seconds because of %d connection refused responses within the past 10 minutes: %s", seconds, CONNECTION_REFUSED_TIMESTAMPS.size(), CONNECTION_REFUSED_TIMESTAMPS.toString());
                 sleep(seconds * SECONDS_IN_MS);
             }
@@ -39,7 +43,7 @@ public class HttpUtil {
                 url = response.headers().allValues("location").get(0);
             try {
                 tries += 1;
-                response = requests.send(HttpRequest.newBuilder(URI.create(url)).build(), HttpResponse.BodyHandlers.ofString());
+                response = requests.send(HttpRequest.newBuilder(URI.create(url)).headers("User-Agent", USER_AGENTS.get((int)Math.round(Math.random() * 2))).build(), HttpResponse.BodyHandlers.ofString());
                 if (response.statusCode() == 200)
                     success = true;
                 else if (response.statusCode() == 404) {
