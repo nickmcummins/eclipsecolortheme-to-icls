09c57937f2db096a90670c6cb3e0b6583bb5ce74
Nick Cummins <nickcummins@microsoft.com>
2021-04-21 20:35:50 -0700

    default dark IDE colors; initial conversion of eclipse -> vscode

--- src/main/java/com/nickmcummins/webscraping/visualstudiocode/VSCodeTokenColorScope.java
+++ src/main/java/com/nickmcummins/webscraping/visualstudiocode/VSCodeTokenColorScope.java
@@ -3,12 +3,25 @@ package com.nickmcummins.webscraping.visualstudiocode;
 import java.util.List;
 
 public enum VSCodeTokenColorScope {
-    COMMENT(List.of("comment", "punctuation.definition.comment", "string.comment"));
+    COMMENT(List.of("comment", "punctuation.definition.comment", "string.comment")),
+    BRACE(List.of("punctuation", "meta.tag.inline.any.html", "meta.tag.block.any.html", "meta.brace")),
+    VARIABLE(List.of("constant", "entity.name.constant", "variable.other.constant", "variable.language", "meta.definition.variable")),
+    ENTITY(List.of("entity", "entity.name")),
+    PARAMETER("variable.parameter.function"),
+    TAG("entity.name.tag"),
+    FUNCTION("entity.name.function"),
+    KEYWORD("keyword"),
+    TYPE(List.of("storage", "storage.type")),
+    STRING(List.of("string", "punctuation.definition.string", "string punctuation.section.embedded source"));
+
 
     private List<String> scope;
 
-    VSCodeTokenColorScope(List<String> scope)
-    {
+    VSCodeTokenColorScope(List<String> scope) {
         this.scope = scope;
     }
-}
+
+    VSCodeTokenColorScope(String scope) {
+        this(List.of(scope));
+    }
+    }
